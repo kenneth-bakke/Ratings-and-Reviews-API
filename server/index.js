5ceb48923408f1e1c554e6ce6022ec49e992d75a
Ubuntu <ubuntu@ip-172-31-3-87.us-west-2.compute.internal>
2022-01-20 21:18:46 +0000

    Fix db connection info

--- server/index.js
+++ server/index.js
@@ -13,9 +13,8 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', router);
-app.get('/', (req, res) => {
-  console.log(__dirname)
-  res.send(path.join(__dirname + '/public/loaderio-a68baefec179e56a0da846de453e9b55.txt'))
+app.get('/loaderio-a68baefec179e56a0da846de453e9b55.txt', (req, res) => {
+  res.send('loaderio-a68baefec179e56a0da846de453e9b55')
 })
 
 server.listen(port, "0.0.0.0", () => {
